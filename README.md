5aa1976cbbb208d13b9f2f9476b50799137aa8c5
Admin <giang@ubuntu.ubuntu-domain>
2019-02-23 23:40:51 +0700

    CRUD Example

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Node.js from scratch
 
 ### Post data from server to view
     https://viblo.asia/p/tu-hoc-nodejs-tu-dau-6J3ZgnGPKmB
+
+### CRUD Example
+    https://viblo.asia/p/nodejs-crud-example-gGJ59vjGKX2
--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const express = require('express');
 const hbs = require('hbs');
 //use bodyParser middleware
 const bodyParser = require('body-parser');
+//use mysql database
+const mysql = require('mysql');
 
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+//Create Connection
+const db = mysql.createConnection({
+  host: hostname,
+  user: 'root',
+  password: 'root',
+  database: 'nodejs'
+});
+
+//connect to database
+db.connect((err) => {
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
 
 //set dynamic views file
 app.set('views',path.join(__dirname,'views'));
@@ -45,12 +61,12 @@ app.post('/post',(req, res) => {
 });
 
 //route for home with params name
-app.get('/:name',(req, res) => {
-  //render index.hbs file
-  res.render('index',{
-    name : req.params.name
-  });
-});
+// app.get('/:name',(req, res) => {
+//   //render index.hbs file
+//   res.render('index',{
+//     name : req.params.name
+//   });
+// });
 
 app.get('/home', function (req, res) {
   res.send('Welcome to Express');
@@ -58,6 +74,30 @@ app.get('/home', function (req, res) {
 
 app.get('/about', function (req, res) {
   res.send('This is about page');
+});
+
+app.get('/users/create',(req, res) => {
+  res.render('users/create');
+});
+
+app.post('/users',(req, res) => {
+  let name = req.body.name;
+  let data = {name: name, email: req.body.email};
+  let sql = "INSERT INTO nodejs.users SET ?";
+  let query = db.query(sql, data, (err, results) => {
+    if(err) throw err;
+    res.redirect('/users');
+  });
+});
+
+app.get('/users',(req, res) => {
+  let sql = "SELECT * FROM users";
+  let query = db.query(sql, (err, users) => {
+    if(err) throw err;
+    res.render('users/index',{
+      users: users
+    });
+  });
 });
 
 app.listen(port, hostname, () => {
